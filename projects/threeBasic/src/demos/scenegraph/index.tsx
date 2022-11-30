0acb126b65af31b6f6e711ea40ac2e2c860f108f
leoChenKg <okcchampion1@gmail.com>
2022-11-30 16:37:09 +0800

    three demo1

--- projects/threeBasic/src/demos/scenegraph/index.tsx
+++ projects/threeBasic/src/demos/scenegraph/index.tsx
@@ -1,3 +1,19 @@
+import { useEffect, createRef } from 'react'
+import { useDemo1 } from './script'
 export default function scenegraph() {
-  return <div>scenegraph</div>
+  const canvas1 = createRef<HTMLCanvasElement>()
+
+  useEffect(() => {
+    useDemo1(canvas1.current!)
+  }, [])
+
+
+  return <div>
+    <canvas style={{
+      width: ' 100%',
+      height: '100%',
+      display: 'block',
+
+    }} ref={canvas1}></canvas>
+  </div >
 }
