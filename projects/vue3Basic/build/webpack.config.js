51355afcd068522325cc5ad4721bb263d957479a
leoChenKg <okcchampion1@gmail.com>
2022-12-09 16:37:00 +0800

    添加 VueBasic 目录

---
+++ projects/vue3Basic/build/webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path')
+const htmlPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+// const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.vue', '.json'],
+    alias: {}
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/] // 要处理 vue 文件中的 ts 代码
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp|svg)$/,
+        type: 'asset',
+        parser: {
+          // 生成Data URI 的条件
+          dataUrlCondition: {
+            // 当资源模块不超过 4kb 时，生成 DataURI，超过 4kb 时，单独打包成文件
+            maxSize: 4 * 1024 // 4b
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new htmlPlugin({
+      template: path.resolve(__dirname, '../public/index.html')
+    })
+    // new CopyPlugin({
+    //   patterns: [
+    //     {
+    //       from: path.resolve(__dirname, '../public/three.js-dev/examples'),
+    //       to: path.resolve(__dirname, './dist/public')
+    //     }
+    //   ]
+    // })
+  ],
+  devServer: {
+    hot: true,
+    port: 3003,
+    historyApiFallback: true,
+    static: {
+      //指定我们的public文件夹为静态资源目录
+      directory: path.join(__dirname, '../public')
+      //指定我们要通过/public访问到directory设置的静态资源
+      //（这个很重要如果不设置默认是通过 / 访问directory设置的静态资源，会和默认访问index.html冲突）
+      // publicPath: '/demos'
+    }
+  }
+}
---
+++ projects/vue3Basic/src/App.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+const a = 10
+</script>
+<template>
+    <div>app {{a}}</div>
+</template>
