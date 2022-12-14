d1a94a606f992fee6eb410744e4e7838d83d41e8
leoChenKg <okcchampion1@gmail.com>
2022-12-14 09:25:49 +0800

    vue basic api

--- projects/vue3Basic/src/App.vue
+++ projects/vue3Basic/src/App.vue
@@ -1,5 +1,32 @@
 <script setup lang="ts">
+import { types } from '@babel/core'
+import { ref, onMounted } from 'vue'
+import setupVue from './demos/setup.vue'
+import setup2Vue from './demos/setup2.vue'
+import refVue from './demos/ref.vue'
+import computedVue from './demos/computed.vue'
+import reactiveVue from './demos/reactive.vue'
+import watchEffectVue from './demos/watchEffect.vue'
+
+// const setup2VueRef = ref<{ add: (n: number | undefined) => void }>()
+
+// onMounted(() => {
+//   console.log(setup2VueRef.value?.add(10))
+// })
+
+// const clickHandler = () => {
+//   console.log('click ahahahah!!')
+// }
 </script>
 <template>
-    
+  <div>
+    <!-- <setupVue @click="clickHandler" :defaultCount="100">
+      <template v-slot:name1> <div>slot name1</div></template>
+    </setupVue>
+    <setup2Vue ref="setup2VueRef" /> -->
+    <!-- <refVue/> -->
+    <!-- <computedVue/> -->
+    <!-- <reactiveVue/> -->
+    <watchEffectVue />
+  </div>
 </template>
---
+++ projects/vue3Basic/src/demos/setup2.vue
@@ -0,0 +1,22 @@
+<script>
+import { h, ref } from 'vue'
+
+export default {
+  setup(props, { expose }) {
+    const count = ref(0)
+    expose({
+      add: (n = 1) => (count.value += n)
+    })
+
+    return () => h('div', count.value)
+  },
+
+  mounted() {
+    console.log(this.count)
+  }
+}
+</script>
+
+<template>
+  <div>setup2</div>
+</template>
